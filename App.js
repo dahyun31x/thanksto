4c09252b19b827ffd41ecb3e1c80620ecb8ec184
Dahyun Kim <dahyun31x@gmail.com>
2023-08-09 17:27:19 +0900

    feat: 텍스트, 버튼 배치

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Text>📝 메모장 앱 📝</Text>
+
+      <Button 
+        title="애플 로그인"
+        accessibilityLabel="애플 로그인"
+      />
+      <Button 
+        title="카카오 로그인"
+        accessibilityLabel="카카오 로그인"
+      />
     </View>
   );
 }
 
+// StatusBar가 꼭 필요한건지
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
