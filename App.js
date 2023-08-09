b83ef8b542e12a38d743ffa33224c3135743c0e7
Dahyun Kim <dahyun31x@gmail.com>
2023-08-09 16:05:46 +0900

    feat: yarn 으로 초기화

--- App.js
+++ App.js
@@ -1,42 +1,20 @@
-
-import React, { useState } from 'react'
-import { StyleSheet } from 'react-native';
-import SignUp from './screens/SignUp';
-
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import SignIn from './screens/SignIn';
-import Profile from './screens/Profile';
-
-const Stack = createNativeStackNavigator(); 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [user, setUser] = useState(null);
-
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="로그인">
-          {(_) => <SignIn user={user} />}
-        </Stack.Screen>
-        <Stack.Screen name="회원가입">
-          {(_) => <SignUp user={user} setUser={setUser} />}
-        </Stack.Screen>
-        <Stack.Screen name="프로필" options={{ title: "Thanks to", headerBackVisible: false }}>
-          {(_) => <Profile user={user} setUser={setUser}/>}
-        </Stack.Screen>
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  title: {
-    fontSize: 16,
-    textAlign: 'center'
-  }
 });
--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,6 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    plugins: [
-      ["module:react-native-dotenv", {
-        "envName": "APP_ENV",
-        "moduleName": "@env",
-        "path": ".env",
-        "safe": false,
-        "allowUndefined": true,
-        "verbose": false
-      }]
-    ]
+    presets: ['babel-preset-expo'],
   };
 };
